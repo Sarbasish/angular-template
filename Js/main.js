8fde082f0934787e38f5c91b7705e41f65aebf86
Sarbasish Mishra <sarbasish.mishra@gmail.com>
2015-09-28 18:44:32 +0530

    Angular js Templates with Nested and parellel templates

---
+++ Js/main.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('Approuter', ['ui.router']);
+app.config(function ($stateProvider, $urlRouterProvider) {
+    
+
+    // For any unmatched url, redirect to home
+    $urlRouterProvider.otherwise("/home");
+    
+    // Now set up the states
+    $stateProvider
+      .state('home', {
+          url: "/home",
+          templateUrl: "templates/home.html"
+      })
+      .state('about', {
+          url: "/about",
+          templateUrl: "templates/about.html"
+      })
+      .state('contact', {
+          url: "/contact",
+          templateUrl: "templates/contact.html"
+      });
+});
+
